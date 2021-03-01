a78e59e2537c1e6968a05bcdaf8a887187751dee
San Yung <sankung93@gmail.com>
2021-03-01 14:09:18 -0800

    finished readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# UC College Town Facts
+<img src=".src/resources/readme.png">
+
+## Table of contents
+* Background and overview
+* Project link
+* Functionality & MVP
+* Architecture and Technologies
+
+# Background and overview
+"A Quick Guide to Coffee" is a scroller with data visualizations of coffee contents ratio, recipe instructions, and nutritional facts for 12 different coffee drinks. The site was created to be user interactive using a scroller feature in which the user can view coffee and corresponding coffee facts bu scrolling up and down.
+# Project link
+Here is a link to the live project: https://sanyung.github.io/A-Quick-Guide-to-Coffee/
+
+# Functionality & MVP
+Users are able to: 
+* Scroll through the visualizations (12 slides)
+* Hover over each of the charts to see more detailed information about the demographics
+* Toggle between chart, recipe instructions, and nutritional facts
+* Click on right hand coffee icon for randomized coffee puns
+* Click over coffee cards to to the right to see a general overview of the coffee drink
+* Toggle between visualizations of coffee item by using the navigation bar to the right
+
+# Architecture and Technologies
+The project uses the following technologies:
+
+* `JavaScript` for scrolling and updating the displayed visualization correctly depending on the location of the website
+* `amCharts` for data visualization
+* `Webpack` for bundling JS files
+
--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -9,95 +9,119 @@ document.getElementsByClassName('types-coffee')[0].addEventListener('scroll', ()
         (document.getElementsByClassName('types-coffee')[0].scrollTop) > 0 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 211)) {
         document.getElementById('expresso-div').style.display = 'block'
+        document.getElementById('espresso-button').style.opacity="1"
     } else {
         document.getElementById('expresso-div').style.display = 'none'
+        document.getElementById('espresso-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 211 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 576)) {
         document.getElementById('redeye-div').style.display = 'block'
+        document.getElementById('redeye-button').style.opacity = "1"
     } else {
         document.getElementById('redeye-div').style.display = 'none'
+        document.getElementById('redeye-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 576 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 906)) {
         document.getElementById('americano-div').style.display = 'block'
+        document.getElementById('americano-button').style.opacity = "1"
     } else {
         document.getElementById('americano-div').style.display = 'none'
+        document.getElementById('americano-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 906 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 1263)) {
         document.getElementById('macchiato-div').style.display = 'block'
+        document.getElementById('macchiato-button').style.opacity = "1"
     } else {
         document.getElementById('macchiato-div').style.display = 'none'
+        document.getElementById('macchiato-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 1263 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 1616)) {
         document.getElementById('cappuccino-div').style.display = 'block'
+        document.getElementById('cappucino-button').style.opacity = "1"
     } else {
         document.getElementById('cappuccino-div').style.display = 'none'
+        document.getElementById('cappucino-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 1616 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 1966)) {
         document.getElementById('flatwhite-div').style.display = 'block'
+        document.getElementById('flatwhite-button').style.opacity = "1"
     } else {
         document.getElementById('flatwhite-div').style.display = 'none'
+        document.getElementById('flatwhite-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 1966 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 2329)) {
         document.getElementById('cafelatte-div').style.display = 'block'
+        document.getElementById('cafelatte-button').style.opacity = "1"
     } else {
         document.getElementById('cafelatte-div').style.display = 'none'
+        document.getElementById('cafelatte-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 2329 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 2678)) {
         document.getElementById('mocha-div').style.display = 'block'
+        document.getElementById('mocha-button').style.opacity = "1"
     } else {
         document.getElementById('mocha-div').style.display = 'none'
+        document.getElementById('mocha-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 2678 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 3016)) {
         document.getElementById('vienna-div').style.display = 'block'
+        document.getElementById('vienna-button').style.opacity = "1"
     } else {
         document.getElementById('vienna-div').style.display = 'none'
+        document.getElementById('vienna-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 3016 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 3297)) {
         document.getElementById('afogato-div').style.display = 'block'
+        document.getElementById('affogato-button').style.opacity = "1"
     } else {
         document.getElementById('afogato-div').style.display = 'none'
+        document.getElementById('affogato-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 3297 &&
         (document.getElementsByClassName('types-coffee')[0].scrollTop < 3526)) {
         document.getElementById('cafeaulait-div').style.display = 'block'
+        document.getElementById('cafeaulait-button').style.opacity = "1"
     } else {
         document.getElementById('cafeaulait-div').style.display = 'none'
+        document.getElementById('cafeaulait-button').style.opacity = "0.5"
     }
 
     if (
         (document.getElementsByClassName('types-coffee')[0].scrollTop) >= 3526) {
         document.getElementById('coldbrew-div').style.display = 'block'
+        document.getElementById('coldbrew-button').style.opacity = "1"
     } else {
         document.getElementById('coldbrew-div').style.display = 'none'
+        document.getElementById('coldbrew-button').style.opacity = "0.5"
     }
 });
 
